cf300582e8164633a508e700fbd4f31c5150d801
AleksLXIV <tikhanin88@bk.ru>
2022-01-28 18:17:07 +0300

    implement firebase utils, includes ability to store authenticated users into our firestore database

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import { Switch, Route } from 'react-router-dom'
 import ShopPage from './pages/shop/shop.component';
 import SingInAndSingPage from './pages/sing-in-and-sing-up/sing-in-and-sing-up.component';
 import Header from './components/header/header';
-import { auth } from './components/firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './components/firebase/firebase.utils';
+
 
 
 
@@ -21,9 +22,25 @@ class App extends React.Component{
 	unsubscribeFromAuth = null
 
 	componentDidMount(){
-		this.unsubscribeFromAuth = auth.onAuthStateChanged(user =>{
-			this.setState({currentUser: user})
-			console.log(user)
+		this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth =>{
+			if(userAuth){
+				const userRef = await createUserProfileDocument(userAuth)
+
+				userRef.onSnapshot(snapShot =>{
+					this.setState({
+						currentUser:{
+							id: snapShot.id,
+							...snapShot.data()
+						}
+					}, () =>{
+						console.log(this.state)
+					})
+					console.log(this.state)
+				})
+			}
+			else{
+				this.setState({currentUser: userAuth})
+			}
 		})
 	}
 
--- src/pages/sing-in-and-sing-up/sing-in-and-sing-up.component.jsx
+++ src/pages/sing-in-and-sing-up/sing-in-and-sing-up.component.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './sing-in-and-sing-up.styles.scss'
 import SingIn from '../../components/sing-in/sing-in.component';
+import SingUp from '../../components/sing-up/sing-up.component';
 
 
 const SingInAndSingPage = () =>(
 	<div className='sing-in-and-sing-up'>
 		<SingIn />
+		<SingUp />
 	</div>
 )
 
