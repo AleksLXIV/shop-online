2301f63350363101ecddbcd62b7dc6f96e4578ca
AleksLXIV <tikhanin88@bk.ru>
2022-04-14 15:48:27 +0300

    added persist-store library

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,13 +1,22 @@
 import { combineReducers } from 'redux'
+import { persistReducer } from 'redux-persist' 
+import storage from 'redux-persist/lib/storage'
 
 import userReducer from './user/user.reducer';
 import cardReducer from './card/card.reducer';
 import postReducer from './posts/posts.reducer';
 
+const persistConfig = {
+	key: 'root',
+	storage,
+	whitelist: ['card']
+}
 
-export default combineReducers({
+const rootReducer = combineReducers({
 	user: userReducer,
 	card: cardReducer,
 	post: postReducer
 	
 })
+
+export default persistReducer(persistConfig, rootReducer)
